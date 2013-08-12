8d7b95ec7f9c77931bed9b793eddcd9d55c19e7f
Andrés Villagrán <andres@villagranquiroz.cl>
2013-08-11 22:09:53 -0400

    fix busqueda

--- cart/controllers/buscador.rb
+++ cart/controllers/buscador.rb
@@ -1,7 +1,6 @@
 VQ::Cart.controllers :buscador do
     post :index, map: 'buscador' do
-        @items =    Product.all(:name.like => "%#{params[:q]}%") + 
-                    Product.all(:description.like => "%#{params[:q]}%") 
+        @items =    Product.all(:name.like => "%#{params[:q]}%", limit: 10)
 
         render 'buscador/index'
     end
